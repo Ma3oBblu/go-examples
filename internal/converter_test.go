ab2a0bf490ce9f44102423c8b0c18f37d62ed4f6
Ruslan Gilyazetdinov <Gilyazetdinov.R@citilink.ru>
2020-10-23 16:53:30 +0300

    [feature] add benchmarks for struct/bool checks in map

--- internal/converter_test.go
+++ internal/converter_test.go
@@ -73,3 +73,77 @@ func TestRegexp(t *testing.T) {
 	var emailRegex = regexp.MustCompile("^(?i)(?:[a-zа-я0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-zа-я0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-zа-я0-9](?:[a-zа-я0-9-]*[a-zа-я0-9])?\\.)+[a-zа-я0-9](?:[a-zа-я0-9-]*[a-zа-я0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-zа-я0-9-]*[a-zа-я0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])$")
 	fmt.Printf("%s", emailRegex)
 }
+
+//BenchmarkBool
+//BenchmarkBool/check_text1_is_exist_in_bool_var
+//BenchmarkBool/check_text1_is_exist_in_bool_var-8         	183924129	         6.49 ns/op
+//BenchmarkBool/check_text3_is_not_exist_in_bool_var
+//BenchmarkBool/check_text3_is_not_exist_in_bool_var-8     	75784028	        15.5 ns/op
+func BenchmarkBool(b *testing.B) {
+	boolVar := map[string]bool{
+		"text1": true,
+		"text2": true,
+	}
+	testsForBool := []struct {
+		name         string
+		checkedValue string
+		boolVar      map[string]bool
+	}{
+		{
+			"check text1 is exist in bool var",
+			"text1",
+			boolVar,
+		},
+		{
+			"check text3 is not exist in bool var",
+			"text3",
+			boolVar,
+		},
+	}
+	for _, tt := range testsForBool {
+		b.Run(tt.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				if _, ok := tt.boolVar[tt.checkedValue]; ok {
+					continue
+				}
+			}
+		})
+	}
+}
+
+//BenchmarkStruct
+//BenchmarkStruct/check_text1_is_exist_in_struct_var
+//BenchmarkStruct/check_text1_is_exist_in_struct_var-8         	187240540	         6.37 ns/op
+//BenchmarkStruct/check_text3_is_not_exist_in_struct_var
+//BenchmarkStruct/check_text3_is_not_exist_in_struct_var-8     	73777120	        15.3 ns/op
+func BenchmarkStruct(b *testing.B) {
+	structVar := map[string]struct{}{
+		"text1": {},
+		"text2": {},
+	}
+	testsForStruct := []struct {
+		name         string
+		checkedValue string
+		structVar    map[string]struct{}
+	}{
+		{
+			"check text1 is exist in struct var",
+			"text1",
+			structVar,
+		},
+		{
+			"check text3 is not exist in struct var",
+			"text3",
+			structVar,
+		},
+	}
+	for _, tt := range testsForStruct {
+		b.Run(tt.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				if _, ok := tt.structVar[tt.checkedValue]; ok {
+					continue
+				}
+			}
+		})
+	}
+}
